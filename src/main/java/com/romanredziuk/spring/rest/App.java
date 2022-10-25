dfa0e6edd571ede3913648a775f3aef88e3a44f5
Roman Redziuk <r8mano@gmail.com>
2022-10-25 17:38:13 +0100

    Final version 1.0

--- src/main/java/com/romanredziuk/spring/rest/App.java
+++ src/main/java/com/romanredziuk/spring/rest/App.java
@@ -1,13 +1,30 @@
 package com.romanredziuk.spring.rest;
 
+import com.romanredziuk.spring.rest.configuration.MyConfig;
+import com.romanredziuk.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee empByID = communication.getEmployee(4);
+//        System.out.println(empByID);
+
+//        Employee employee = new Employee("Carey", "Watkins", "HR", 2500);
+//        employee.setId(9);
+//        communication.saveEmployee(employee);
+
+        communication.deleteEmployee(9);
     }
 }
